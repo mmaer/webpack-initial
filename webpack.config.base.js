c378bf32a5fdfd020a692d9c83649e720172500c
Kamil Kruczek <kamilkruczek0@hotmail.com>
2019-05-18 18:50:36 +0200

    Initial commit

---
+++ webpack.config.base.js
@@ -0,0 +1,80 @@
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: [
+            './src/main/index',
+        ],
+    },
+    output: {
+        path: resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'window.DEV_TOOLS_ENABLED': JSON.stringify(true),
+            'process.env': {
+                NODE_ENV: JSON.stringify('develop'),
+            },
+        }),
+        new SpriteLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/main/index.html',
+        }),
+    ],
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            include: resolve(__dirname),
+            exclude: /node_modules/,
+        }, {
+            test: /\.*css$/,
+            exclude: /node_modules/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader',
+            ],
+        }, {
+            test: /\.svg$/,
+            use: [{
+                loader: 'svg-sprite-loader',
+                options: {
+                    extract: true,
+                },
+            }, {
+                loader: 'svgo-loader',
+                options: {
+                    plugins: [
+                        { removeTitle: true },
+                        { convertColors: { shorthex: false } },
+                        { convertPathData: false },
+                    ],
+                },
+            }],
+        }, {
+            test: /\.icons\.(js|json)$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', 'postcss-loader', 'sass-loader', 'webfonts-loader'],
+            }),
+        }, {
+            test: /\.(png|jpg|eot|woff|woff2|ttf)$/,
+            use: 'file-loader?',
+        }, {
+            test: /\.(txt)$/,
+            use: 'raw-loader',
+        }, {
+            test: /\.html$/,
+            use: 'raw-loader',
+        }],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css', '.scss', '.json'],
+    },
+};
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,127 +1,48 @@
-const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
-const { join, resolve } = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const merge = require('webpack-merge');
+const { join } = require('path');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
-module.exports = {
-    devtool: 'source-map',
-    entry: {
-        app: [
-            './src/main/index',
-        ],
-    },
-    output: {
-        path: resolve(__dirname, 'dist', 'www'),
-        filename: '[name].js',
-    },
+const baseConfig = require('./webpack.config.base');
+
+module.exports = merge(baseConfig, {
+    mode: 'production',
     plugins: [
-        new webpack.DefinePlugin({
-            'window.DEV_TOOLS_ENABLED': JSON.stringify(true),
-            'process.env': {
-                NODE_ENV: JSON.stringify('production'),
-            },
-        }),
+        ...baseConfig.plugins,
+        new webpack.ProgressPlugin(),
         new webpack.optimize.OccurrenceOrderPlugin(),
-        new CleanWebpackPlugin(['dist/www'], {
-            root: __dirname,
-        }),
-        new SpriteLoaderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            mangle: true,
-            compress: {
-                warnings: true,
-                screw_ie8: true,
-                conditionals: true,
-                unused: true,
-                comparisons: true,
-                sequences: true,
-                dead_code: true,
-                evaluate: true,
-                if_return: true,
-                join_vars: true,
-            },
-            output: {
-                comments: false,
-            },
-        }),
+        new CleanWebpackPlugin(),
         new webpack.optimize.ModuleConcatenationPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/main/index.html',
-        }),
-        new ExtractTextPlugin('[name].css'),
         new CopyWebpackPlugin([{
             from: join(__dirname, 'src/main/index.html'),
-            to: join(__dirname, 'dist', 'www', 'index.html'),
+            to: join(__dirname, 'dist', 'index.html'),
         }]),
+        new BundleAnalyzerPlugin(),
     ],
-    module: {
-        rules: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            include: resolve(__dirname),
-            exclude: /node_modules/,
-        }, {
-            test: /\.*css$/,
-            exclude: /node_modules/,
-            use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: [{
-                    loader: 'css-loader',
-                }, {
-                    loader: 'postcss-loader',
-                    options: {
-                        plugins: () => [autoprefixer],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                sourceMap: true,
+                uglifyOptions: {
+                    mangle: true,
+                    compress: {
+                        conditionals: true,
+                        unused: true,
+                        comparisons: true,
+                        sequences: true,
+                        dead_code: true,
+                        evaluate: true,
+                        if_return: true,
+                        join_vars: true,
                     },
-                }, {
-                    loader: 'sass-loader',
-                }],
-            }),
-        }, {
-            test: /\.svg$/,
-            use: [{
-                loader: 'svg-sprite-loader',
-                options: {
-                    extract: true,
+                    output: {
+                        comments: false,
+                    },
                 },
-            }, {
-                loader: 'svgo-loader',
-                options: {
-                    plugins: [
-                        { removeTitle: true },
-                        { convertColors: { shorthex: false } },
-                        { convertPathData: false },
-                    ],
-                },
-            }],
-        }, {
-            test: /\.icons\.(js|json)$/,
-            use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: ['css-loader', 'postcss-loader', 'sass-loader', 'webfonts-loader'],
             }),
-        }, {
-            test: /\.(png|jpg|eot|woff|woff2|ttf)$/,
-            use: 'file-loader?',
-        }, {
-            test: /\.(txt)$/,
-            use: 'raw-loader',
-        }, {
-            test: /\.html$/,
-            use: 'raw-loader',
-        }],
+        ],
     },
-    resolve: {
-        extensions: ['.js', '.css', '.scss', '.json'],
-    },
-    devServer: {
-        disableHostCheck: true,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-        },
-    },
-};
+});
